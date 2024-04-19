08e3e2a450ba08404ba4d519de6e06f5288f197c
MURILO DA SILVA <murilo_da-silva@estudante.sc.senai.br>
2024-04-19 19:53:51 -0300

    trabalho

--- algoritmos_e_programacao/trabalho_01/main.py
+++ algoritmos_e_programacao/trabalho_01/main.py
@@ -1,25 +1,53 @@
-import stock
+#aqui definimos nossa base de dados em memoria, é um tipo de variavel que serve como um lista, onde vamos inserir nossos produtos
+cellPhones = {
+  "IPHONE 15": 10,
+  "MOTOROLA G5": 7
+}
 
-menuActions = {
-  "1": stock.check,
-  "2": stock.insert,
-  "3" : stock.remove,
-  "0": quit
-} 
+# função check, faz a impressão na tela do que possui na nossa base dados
+def check():
+  print("------------------------------------------ Celulares ----------------------------------------------------------")
+  pattern = '{: >20} {: >25}'       
+  print(pattern.format('Modelo','Quantidade'))
+  print("")  
+  for key, value in cellPhones.items():
+    print(f'{key :>20} {value :>25}')
+  print("---------------------------------------------------------------------------------------------------------------")  
 
-menus = [
-    "1 - Conferir estoque",
-    "2 - Inserir celular",
-    "3 - Saida celular",    
-    "0 - Sair"
-]
+# funcao insert, faz a inclusao do produto na base de dados
+def insert():
+    model = input("Digite o modelo: ").upper()
+    quantitity = int(input("Digite a quantidade: "))
+    if model in cellPhones:
+      quantitity += cellPhones[model]
+      
+    cellPhones.update({model: quantitity})
+    
+# funcao remove, faz a exclusao do produto na base de dados    
+def remove():
+    model = input("Digite o modelo: ")
+    quantitity = int(input("Digite a quantidade: "))
+    cellPhones.update({model: quantitity})
 
-while True:
-    for menu in menus:
-        print(menu, end="\n")
+# aqui definimos a funcao que cria o menu
+menus = {}
+def menuAdd(option, name, func):
+    menuName = f"{option} - {name}"
+    menus[option] = [menuName, func]
+
+# aqui criamos a sequencia dos menus seguido de digito, nome do menu, e a funcao que o menu executa
+menuAdd("1", "Conferir estoque.", check)
+menuAdd("2", "Inserir celular.", insert)
+menuAdd("3", "Saida celular.", remove)
+menuAdd("0", "Sair.", quit)
+
+# aqui exibe os menus no console
+while True:        
+    for key, value in menus.items():
+        print(value[0], end="\n")
     key = input("Digite o numero do menu: ")    
-    if key in menuActions:
-        action = menuActions[key]
-        action()
+    if key in menus:
+        action = menus[key]
+        action[1]()
     else:
-         print("Menu invalido: %s" %action)
+         print("Menu invalido: %s" %key)
